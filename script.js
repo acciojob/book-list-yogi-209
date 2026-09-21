ba012e52e77ccea027291813a3701a660b7b402a
Boyanapalli Yogendra sai <boyanapalliyogendrasai@gmail.com>
2026-09-21 12:34:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded",()=>{
 	});
 bookList.addEventListener("click",(e)=>{
 if(e.target.classList.contains("delete")){
-	e.target.parentElement.parentElement.remove();
+	e.target.parentElement.remove();
 }
 });
 });
